7c296163f7a416cd5a7638586d09bc0d4129c867
Divyangi <divyangi.chaudhari@iitgn.ac.in>
2018-06-20 15:56:26 +0530

    after nav added

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,34 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from "styled-components";
 
+
+const HeaderWrapper = styled.div`
+  background: #524763;
+  margin-bottom: 1.45rem;
+  height: 60vh;
+  overflow: hidden;
+  position: relative;
+  height: 70vh;
+  h1 {
+    img {
+      height: 80px;
+    }
+  }
+`;
+
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+  position: relative;
+  z-index: 2;
+  display:flex;
+  justify-content:space-between;
+`;
 const Header = ({ siteTitle }) => (
+  <HeaderWrapper>
+    <HeaderContainer>
   <div
     style={{
       background: 'rebeccapurple',
@@ -28,6 +55,18 @@ const Header = ({ siteTitle }) => (
       </h1>
     </div>
   </div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/page-2">About</Link>
+        </li>
+      </ul>
+    </nav>
+    </HeaderContainer>
+  </HeaderWrapper>
 )
 
 export default Header
